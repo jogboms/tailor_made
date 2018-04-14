db80c3cebef879b410dddc41794a0f1b0ce955bb
Jogboms <jeremiahogbomo@gmail.com>
2018-04-14 11:11:47 +0100

    chore: code clean up

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:tailor_made/utils/tm_colors.dart';
 import 'package:tailor_made/utils/tm_fonts.dart';
 import 'package:tailor_made/pages/homepage.dart';
+import 'package:tailor_made/utils/tm_strings.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'TailorMade',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        accentColor: TMColors.primary,
-        primarySwatch: TMColors.white,
-        fontFamily: TMFonts.raleway,
+void main() => runApp(
+      MaterialApp(
+        title: TMStrings.appName,
+        theme: new ThemeData(
+          accentColor: TMColors.primary,
+          primarySwatch: TMColors.white,
+          fontFamily: TMFonts.raleway,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
