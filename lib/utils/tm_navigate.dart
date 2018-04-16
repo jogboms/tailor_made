6dea05180f2d9cfdee2ca29fbdbe701002ed3ff3
Jogboms <jeremiahogbomo@gmail.com>
2018-04-16 16:20:20 +0100

    refactor: tm_navigate w/ custom PageRoute

--- lib/utils/tm_navigate.dart
+++ lib/utils/tm_navigate.dart
@@ -3,19 +3,36 @@ import 'package:flutter/material.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
 
 class TMNavigate {
-  TMNavigate._();
-
-  static void android(BuildContext context, Widget widget) {
+  TMNavigate(BuildContext context, Widget widget) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (BuildContext context) => TMTheme(child: widget)),
+      TMNavigateRoute(builder: (BuildContext context) => TMTheme(child: widget)),
     );
   }
+}
 
-  static void ios(BuildContext context, Widget widget) {
-    Navigator.push(
-      context,
-      CupertinoPageRoute(builder: (BuildContext context) => TMTheme(child: widget)),
-    );
+class TMNavigateRoute<T> extends MaterialPageRoute<T> {
+  TMNavigateRoute({WidgetBuilder builder, RouteSettings settings}) : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    Animation<Offset> positionIn(Animation<double> animation) => new Tween<Offset>(
+          begin: const Offset(1.0, 0.0),
+          end: Offset.zero,
+        ).animate(animation);
+
+    // Animation<Offset> positionOut(Animation<double> animation) => new Tween<Offset>(
+    //       begin: Offset.zero,
+    //       end: Offset.zero,
+    //     ).animate(animation);
+
+    if (settings.isInitialRoute) return child;
+
+    // return new FadeTransition(opacity: animation, child: child);
+    return new SlideTransition(position: positionIn(animation), child: child);
+    // return new SlideTransition(
+    //   position: positionIn(animation),
+    //   child: new SlideTransition(position: positionOut(secondaryAnimation), child: child),
+    // );
   }
 }
