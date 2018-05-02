c5360d4a6993065a0cf120b8770aa9c14e86823f
Jogboms <jeremiahogbomo@gmail.com>
2018-05-02 16:20:53 +0100

    refactor: allow for other configurations

--- lib/ui/app_bar.dart
+++ lib/ui/app_bar.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:tailor_made/ui/back_button.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
 
-AppBar appBar(BuildContext context, {String title, List<Widget> actions}) {
+AppBar appBar(
+  BuildContext context, {
+  String title: "",
+  List<Widget> actions,
+  double elevation: 1.0,
+  bool centerTitle: false,
+}) {
   final TMTheme theme = TMTheme.of(context);
   return AppBar(
-    elevation: 1.0,
+    elevation: elevation,
     backgroundColor: theme.appBarBackgroundColor,
     leading: backButton(context),
-    centerTitle: false,
+    centerTitle: centerTitle,
     title: new Text(
       title,
       style: theme.appBarStyle.copyWith(
