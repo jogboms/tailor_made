3f74e2e3da080b48ccf04678b9f2e3b025c46d06
Jogboms <jeremiahogbomo@gmail.com>
2018-05-03 16:39:21 +0100

    chore: implement gallery_grid_item

--- lib/pages/gallery/gallery.dart
+++ lib/pages/gallery/gallery.dart
@@ -1,8 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
-import 'gallery_view.dart';
+import 'package:tailor_made/pages/gallery/gallery_grid_item.dart';
 
 class Gallery extends StatelessWidget {
   @override
@@ -27,39 +25,7 @@ class Gallery extends StatelessWidget {
             crossAxisCount: 3,
             crossAxisSpacing: 1.5,
             mainAxisSpacing: 1.5,
-            children: List.generate(40, (i) {
-              Random rand = new Random();
-              int id = rand.nextInt(10000);
-              int id2 = rand.nextInt(10000);
-              const image = "https://placeimg.com/640/640/tech";
-              return new Hero(
-                tag: "-$image-$id-$id2",
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: new NetworkImage(image),
-                    ),
-                  ),
-                  child: new Material(
-                    color: Colors.transparent,
-                    child: new InkWell(
-                      onTap: () {
-                        PageRouteBuilder pageBuilder = new PageRouteBuilder(
-                          opaque: false,
-                          pageBuilder: (BuildContext context, _, __) => GalleryView(image, "-$image-$id-$id2"),
-                          transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
-                            return new FadeTransition(opacity: animation, child: child);
-                          },
-                        );
-                        Navigator.of(context).push(pageBuilder);
-                      },
-                    ),
-                  ),
-                ),
-              );
-            }).toList(),
+            children: List.generate(40, (i) => GalleryGridItem()).toList(),
           ),
         ],
       ),
