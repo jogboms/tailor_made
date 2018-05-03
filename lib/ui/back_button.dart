a84b5650fe274e54bcc93532fa57827506a31e1d
Jogboms <jeremiahogbomo@gmail.com>
2018-05-03 09:57:20 +0100

    refactor(ui): configurable backButton color

--- lib/ui/back_button.dart
+++ lib/ui/back_button.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
 
-IconButton backButton(BuildContext context) {
+IconButton backButton(BuildContext context, {Color color}) {
   final TMTheme theme = TMTheme.of(context);
   return new IconButton(
     icon: new Icon(
       Icons.arrow_back,
-      color: theme.appBarColor,
+      color: color != null ? color : theme.appBarColor,
     ),
     onPressed: () => Navigator.maybePop(context),
   );
