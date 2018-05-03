a02026dad0e660cdd5f2c665000627df15f0dcb3
Jogboms <jeremiahogbomo@gmail.com>
2018-05-03 12:56:22 +0100

    refactor(jobs): redesign single job UI

--- lib/pages/jobs/job.dart
+++ lib/pages/jobs/job.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tailor_made/ui/app_bar.dart';
+import 'package:flutter/rendering.dart';
+import 'package:tailor_made/ui/back_button.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'ui/payment_grids.dart';
@@ -22,7 +23,6 @@ class _JobPageState extends State<JobPage> {
   @override
   Widget build(BuildContext context) {
     final TMTheme theme = TMTheme.of(context);
-    final _theme = Theme.of(context);
 
     final List<Measure> items = <Measure>[
       new Measure(name: "Arm Hole", measurement: 29, unit: "In"),
@@ -36,62 +36,191 @@ class _JobPageState extends State<JobPage> {
       new Measure(name: "Knee Length", measurement: 4, unit: "In"),
     ];
 
-    Widget header = Container(
-      // color: Colors.grey[200],
-      child: new SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new Padding(
-              padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
-              child: Text("Indian Blouse", style: _theme.textTheme.headline),
+    Widget header = new Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        new Padding(
+          padding: const EdgeInsets.only(top: 4.0, left: 24.0, bottom: 4.0, right: 24.0),
+          child: Text(
+            "Indian Blouse with Chifon Top and Mexican Trouser",
+            style: ralewayLight(20.0, Colors.white),
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            overflow: TextOverflow.fade,
+          ),
+        ),
+        new Padding(
+          padding: const EdgeInsets.only(top: 4.0, bottom: 16.0),
+          child: Text(
+            "₦20,000",
+            style: ralewayLight(35.0, Colors.white).copyWith(
+              letterSpacing: 1.25,
             ),
-            // const Divider(height: 1.0),
-            new Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: items.map((Measure item) {
-                  return new Container(
-                    color: Colors.grey[100],
-                    padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                    margin: const EdgeInsets.symmetric(vertical: 1.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Expanded(child: Text(item.name, style: ralewayMedium(14.0))),
-                        Text("${item.measurement} ", style: ralewayBold(16.0)),
-                        Text(item.unit, style: ralewayLight(12.0)),
-                      ],
+            textAlign: TextAlign.center,
+          ),
+        ),
+        new Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(bottom: TMBorderSide()),
+          ),
+          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: new Container(
+                  decoration: BoxDecoration(
+                    border: Border(right: TMBorderSide()),
+                  ),
+                  child: new Column(
+                    children: <Widget>[
+                      Text(
+                        "PAID",
+                        style: ralewayLight(8.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "₦16,500",
+                        style: ralewayMedium(18.0, Colors.greenAccent).copyWith(
+                          letterSpacing: 1.25,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: new Column(
+                  children: <Widget>[
+                    Text(
+                      "UNPAID",
+                      style: ralewayLight(8.0),
+                      textAlign: TextAlign.center,
                     ),
-                  );
-                }).toList(),
+                    Text(
+                      "₦3,500",
+                      style: ralewayMedium(18.0, Colors.redAccent).copyWith(
+                        letterSpacing: 1.25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
+    Widget list = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: items.map((Measure item) => new MeasureItem(item)).toList(),
+    );
+
+    Widget appBar = new Row(
+      children: <Widget>[
+        new CircleAvatar(
+          backgroundColor: Colors.white,
+          backgroundImage: NetworkImage("https://placeimg.com/640/640/people"),
+        ),
+        SizedBox(width: 8.0),
+        new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Chioma Coker", style: ralewayBold(20.0, Colors.white)),
+            new Text.rich(
+              new TextSpan(
+                children: [
+                  new TextSpan(
+                    text: "Sunday",
+                    style: new TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  new TextSpan(text: ", 12"),
+                  new TextSpan(
+                    text: "nd",
+                    style: new TextStyle(fontSize: 12.0),
+                  ),
+                  new TextSpan(text: " March"),
+                ],
+              ),
+              style: new TextStyle(
+                color: Colors.white,
+                fontSize: 12.0,
               ),
             ),
           ],
         ),
-      ),
+      ],
     );
 
     return new Scaffold(
       backgroundColor: theme.scaffoldColor,
-      appBar: appBar(context, elevation: 0.0),
-      body: new SafeArea(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            new Expanded(child: header),
-            new Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: GalleryGrids(),
+      body: new NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 250.0,
+              flexibleSpace: FlexibleSpaceBar(background: header),
+              pinned: true,
+              titleSpacing: 0.0,
+              elevation: 0.0,
+              automaticallyImplyLeading: false,
+              centerTitle: false,
+              backgroundColor: accentColor,
+              leading: backButton(context, color: Colors.white),
+              title: appBar,
             ),
-            // const Divider(height: 1.0),
-            new Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: PaymentGrids(),
+          ];
+        },
+        body: new SafeArea(
+          top: false,
+          child: new SingleChildScrollView(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: list,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: GalleryGrids(),
+                ),
+                // const Divider(height: 1.0),
+                new Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: PaymentGrids(),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class MeasureItem extends StatelessWidget {
+  final Measure item;
+  MeasureItem(this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.grey[100].withOpacity(.5),
+      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+      margin: const EdgeInsets.symmetric(vertical: 1.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(child: Text(item.name, style: ralewayMedium(14.0, titleBaseColor))),
+          Text("${item.measurement} ", style: ralewayRegular(16.0, titleBaseColor)),
+          Text(item.unit, style: ralewayLight(12.0, titleBaseColor)),
+        ],
       ),
     );
   }
