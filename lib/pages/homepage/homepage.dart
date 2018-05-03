7277ff876c4a13294a0f432daeafde2bed39d07f
Jogboms <jeremiahogbomo@gmail.com>
2018-05-03 17:08:19 +0100

    refactor: redesign homepage to accomodate gallery

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -1,11 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tailor_made/pages/homepage/ui/bottom_row.dart';
 import 'package:tailor_made/pages/homepage/ui/header.dart';
+import 'package:tailor_made/pages/homepage/ui/helpers.dart';
 import 'package:tailor_made/pages/homepage/ui/stats.dart';
 import 'package:tailor_made/pages/homepage/ui/top_row.dart';
 import 'package:tailor_made/pages/accounts/accounts.dart';
 import 'package:tailor_made/utils/tm_navigate.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
+import 'package:tailor_made/pages/contacts/contacts_create.dart';
+import 'package:tailor_made/pages/jobs/jobs_create.dart';
+import 'package:tailor_made/pages/payments/payments_create.dart';
+
+enum CreateOptions {
+  clients,
+  projects,
+  payments,
+}
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,6 +28,67 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final TMTheme theme = TMTheme.of(context);
+
+    Future onTapCreate() async {
+      CreateOptions result = await showDialog<CreateOptions>(
+        context: context,
+        builder: (BuildContext context) {
+          return new SimpleDialog(
+            title: const Text('Select action', style: const TextStyle(fontSize: 14.0)),
+            children: <Widget>[
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, CreateOptions.clients);
+                },
+                child: TMListTile(
+                  color: Colors.orangeAccent,
+                  icon: Icons.supervisor_account,
+                  title: "Clients",
+                ),
+              ),
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, CreateOptions.projects);
+                },
+                child: TMListTile(
+                  color: Colors.greenAccent.shade400,
+                  icon: Icons.attach_money,
+                  title: "Projects",
+                ),
+              ),
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, CreateOptions.payments);
+                },
+                child: TMListTile(
+                  color: Colors.redAccent,
+                  icon: Icons.usb,
+                  title: "Payments",
+                ),
+              ),
+            ],
+          );
+        },
+      );
+      switch (result) {
+        case CreateOptions.clients:
+          {
+            TMNavigate(context, ContactsCreatePage());
+            break;
+          }
+        case CreateOptions.projects:
+          {
+            TMNavigate(context, JobsCreatePage());
+            break;
+          }
+        case CreateOptions.payments:
+          {
+            TMNavigate(context, PaymentsCreatePage());
+            break;
+          }
+      }
+    }
+
     return new Scaffold(
       backgroundColor: theme.scaffoldColor,
       appBar: AppBar(
@@ -38,6 +111,14 @@ class _HomePageState extends State<HomePage> {
           StatsWidget(),
           TopRowWidget(),
           BottomRowWidget(),
+          new FlatButton(
+            padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+            child: new Text(
+              "CREATE",
+              style: ralewayMedium(14.0, theme.textMutedColor),
+            ),
+            onPressed: onTapCreate,
+          )
         ],
       ),
     );
--- lib/pages/homepage/ui/bottom_row.dart
+++ lib/pages/homepage/ui/bottom_row.dart
@@ -1,87 +1,19 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:tailor_made/pages/homepage/ui/helpers.dart';
 import 'package:tailor_made/pages/payments/payments.dart';
-import 'package:tailor_made/pages/contacts/contacts_create.dart';
-import 'package:tailor_made/pages/jobs/jobs_create.dart';
-import 'package:tailor_made/pages/payments/payments_create.dart';
+import 'package:tailor_made/pages/gallery/gallery.dart';
 import 'package:tailor_made/utils/tm_navigate.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
-
-enum CreateOptions {
-  clients,
-  projects,
-  payments,
-}
 
 class BottomRowWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final TMTheme theme = TMTheme.of(context);
-
-    Future onTapCreate() async {
-      CreateOptions result = await showDialog<CreateOptions>(
-        context: context,
-        builder: (BuildContext context) {
-          return new SimpleDialog(
-            title: const Text('Select action', style: const TextStyle(fontSize: 14.0)),
-            children: <Widget>[
-              new SimpleDialogOption(
-                onPressed: () {
-                  Navigator.pop(context, CreateOptions.clients);
-                },
-                child: TMListTile(
-                  color: Colors.orangeAccent,
-                  icon: Icons.supervisor_account,
-                  title: "Clients",
-                ),
-              ),
-              new SimpleDialogOption(
-                onPressed: () {
-                  Navigator.pop(context, CreateOptions.projects);
-                },
-                child: TMListTile(
-                  color: Colors.greenAccent.shade400,
-                  icon: Icons.attach_money,
-                  title: "Projects",
-                ),
-              ),
-              new SimpleDialogOption(
-                onPressed: () {
-                  Navigator.pop(context, CreateOptions.payments);
-                },
-                child: TMListTile(
-                  color: Colors.redAccent,
-                  icon: Icons.usb,
-                  title: "Payments",
-                ),
-              ),
-            ],
-          );
-        },
-      );
-      switch (result) {
-        case CreateOptions.clients:
-          {
-            TMNavigate(context, ContactsCreatePage());
-            break;
-          }
-        case CreateOptions.projects:
-          {
-            TMNavigate(context, JobsCreatePage());
-            break;
-          }
-        case CreateOptions.payments:
-          {
-            TMNavigate(context, PaymentsCreatePage());
-            break;
-          }
-      }
-    }
-
     void onTapPayments() {
       TMNavigate(context, PaymentsPage());
+    }
+
+    void onTapGallery() {
+      TMNavigate(context, GalleryPage());
     }
 
     return new Container(
@@ -108,26 +40,14 @@ class BottomRowWidget extends StatelessWidget {
             ),
           ),
           new Expanded(
-            child: new FlatButton(
-              child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Icon(
-                    Icons.add,
-                    size: 38.0,
-                    color: theme.textMutedColor,
-                  ),
-                  new Text(
-                    "CREATE",
-                    style: new TextStyle(
-                      color: theme.textMutedColor,
-                      fontSize: 10.0,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  )
-                ],
+            child: new Container(
+              child: TMGridTile(
+                color: Colors.blueAccent,
+                icon: Icons.image,
+                title: "Gallery",
+                subTitle: "206 Photos",
+                onPressed: onTapGallery,
               ),
-              onPressed: onTapCreate,
             ),
           ),
         ],
