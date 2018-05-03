7a79b516179236d601a01317a6b8c537c163275b
Jogboms <jeremiahogbomo@gmail.com>
2018-05-03 16:53:24 +0100

    refactor(gallery): renmae Gallery -> GalleryPage

--- lib/pages/gallery/gallery.dart
+++ lib/pages/gallery/gallery.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tailor_made/utils/tm_theme.dart';
 import 'package:tailor_made/pages/gallery/gallery_grid_item.dart';
 
-class Gallery extends StatelessWidget {
+class GalleryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TMTheme theme = TMTheme.of(context);
