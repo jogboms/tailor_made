0e6f98c0fa6b186c3f3d9f83b4361fdb24ce8709
Jogboms <jeremiahogbomo@gmail.com>
2018-07-02 16:00:19 +0100

    chore: enable gallery

--- lib/pages/contacts/ui/contact_gallery_grid.dart
+++ lib/pages/contacts/ui/contact_gallery_grid.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tailor_made/pages/contacts/models/contact.model.dart';
 import 'package:tailor_made/pages/gallery/models/gallery_image.model.dart';
 import 'package:tailor_made/pages/gallery/gallery_grid.dart';
+import 'package:tailor_made/pages/jobs/models/job.model.dart';
+import 'package:tailor_made/services/cloudstore.dart';
+import 'package:tailor_made/ui/tm_empty_result.dart';
+import 'package:tailor_made/ui/tm_loading_spinner.dart';
 
 class GalleryGridWidget extends StatelessWidget {
   final ContactModel contact;
@@ -10,13 +15,40 @@ class GalleryGridWidget extends StatelessWidget {
 
   @override
   build(BuildContext context) {
-    return GalleryGrid(
-      images: List
-          .generate(
-            40,
-            (int index) => GalleryImageModel(src: "https://placeimg.com/640/640/tech/$index"),
-          )
-          .toList(),
+    print(contact.toMap());
+    return StreamBuilder(
+      stream: Cloudstore.jobs.where("contact.id", isEqualTo: contact.id).snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SliverFillRemaining(
+            child: loadingSpinner(),
+          );
+        }
+
+        List<DocumentSnapshot> list = snapshot.data.documents;
+
+        if (list.isEmpty) {
+          return SliverFillRemaining(
+            child: TMEmptyResult(message: "No images available"),
+          );
+        }
+
+        final List<GalleryImageModel> images = [];
+
+        final jobs = list.map((item) => JobModel.fromJson(item.data));
+
+        jobs.forEach(
+          (item) => images.addAll(
+                item.images.map(
+                  (src) => GalleryImageModel(src: src),
+                ),
+              ),
+        );
+
+        return GalleryGrid(
+          images: images.toList(),
+        );
+      },
     );
   }
 }
