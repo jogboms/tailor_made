ad103e0f8e04bf5b45888537b55546b6f21bf38f
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2018-07-08 02:47:08 +0100

    chore: cached images (#29)
    * chore: add dependencies
    
    * feat: implementation

--- lib/ui/circle_avatar.dart
+++ lib/ui/circle_avatar.dart
@@ -1,10 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 CircleAvatar circleAvatar({double radius, String imageUrl}) {
   return new CircleAvatar(
     radius: radius ?? null,
     backgroundColor: imageUrl != null ? Colors.grey.shade400 : Colors.white,
-    backgroundImage: imageUrl != null ? NetworkImage(imageUrl) : null,
+    backgroundImage: imageUrl != null ? CachedNetworkImageProvider(imageUrl) : null,
     child: imageUrl != null ? null : Center(child: Icon(Icons.person_outline)),
   );
 }
