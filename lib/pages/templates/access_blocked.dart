df7535fc2345f91db252602bdbed5b3d4339c882
Jogboms <jeremiahogbomo@gmail.com>
2018-07-16 22:21:22 +0100

    chore: base template setup

---
+++ lib/pages/templates/access_blocked.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AccessBlockedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[],
+      ),
+    );
+  }
+}
---
+++ lib/pages/templates/rate_limit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RateLimitPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[],
+      ),
+    );
+  }
+}
