94d5d3ce8d321a3adb2d2fcec997dac69d108fda
Jogboms <jeremiahogbomo@gmail.com>
2018-07-24 11:11:55 +0100

    refactor(redux): added settings

--- lib/redux/states/main.dart
+++ lib/redux/states/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:tailor_made/redux/states/account.dart';
 import 'package:tailor_made/redux/states/contacts.dart';
 import 'package:tailor_made/redux/states/jobs.dart';
+import 'package:tailor_made/redux/states/settings.dart';
 import 'package:tailor_made/redux/states/stats.dart';
 
 @immutable
@@ -11,18 +12,21 @@ class ReduxState {
   final JobsState jobs;
   final StatsState stats;
   final AccountState account;
+  final SettingsState settings;
 
   const ReduxState({
     @required this.contacts,
     @required this.jobs,
     @required this.stats,
     @required this.account,
+    @required this.settings,
   });
 
   const ReduxState.initialState()
       : contacts = const ContactsState.initialState(),
         jobs = const JobsState.initialState(),
         account = const AccountState.initialState(),
+        settings = const SettingsState.initialState(),
         stats = const StatsState.initialState();
 
   ReduxState copyWith({
@@ -30,12 +34,14 @@ class ReduxState {
     JobsState jobs,
     StatsState stats,
     AccountState account,
+    SettingsState settings,
   }) {
     return new ReduxState(
       contacts: contacts ?? this.contacts,
       jobs: jobs ?? this.jobs,
       stats: stats ?? this.stats,
       account: account ?? this.account,
+      settings: settings ?? this.settings,
     );
   }
 }
