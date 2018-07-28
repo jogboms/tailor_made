63a81d682ed401367c99d6d94810b1ead7a51287
Jogboms <jeremiahogbomo@gmail.com>
2018-07-28 13:31:42 +0100

    test: group_model_by

---
+++ test/utils_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tailor_made/models/measure.dart';
+import 'package:tailor_made/utils/tm_group_model_by.dart';
+
+void main() {
+  group("group_mode_by", () {
+    test('test group_mode_by', () {
+      final _date = DateTime.now();
+      const _id = "id_123456", _unit = "In";
+      final List<MeasureModel> measures = [
+        new MeasureModel(
+          group: "Blouse",
+          name: "length",
+          id: _id,
+          unit: _unit,
+          createdAt: _date,
+        ),
+        new MeasureModel(
+          group: "Blouse",
+          name: "Waist",
+          id: _id,
+          unit: _unit,
+          createdAt: _date,
+        ),
+        new MeasureModel(
+          group: "Trouser",
+          name: "Waist",
+          id: _id,
+          unit: _unit,
+          createdAt: _date,
+        ),
+        new MeasureModel(
+          group: "Trouser",
+          name: "length",
+          id: _id,
+          unit: _unit,
+          createdAt: _date,
+        ),
+        new MeasureModel(
+          group: "Blouse",
+          name: "Arm",
+          id: _id,
+          unit: _unit,
+          createdAt: _date,
+        ),
+      ];
+
+      final grouped = groupModelBy<MeasureModel>(
+        measures,
+        (measure) => measure.group,
+      );
+
+      expect(grouped.isNotEmpty, true);
+      expect(grouped['Blouse'].length, 3);
+      expect(grouped['Trouser'].length, 2);
+    });
+  });
+}
